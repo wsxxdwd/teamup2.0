6c7d00f3f08a11afe9040edeabae7587c51bb65c
wsxxdwd <wsxxdwdd@gmail.com>
2013-11-01 18:39:59 +0800

    rebuilt login and register and update .gitignore

--- js/login.js
+++ js/login.js
@@ -2,6 +2,18 @@ $(document).ready(function(){
 	$(".login_ipt input").keyup(function(){
 		placeholder_hack(this);
 	});
+	$("#main").on("click","#register_tab",function(){
+		$("#login_tab").transition({y:30});
+		$("#register_tab").transition({y:-30});
+		$("#login_tab").css("background","url(./img/login/login_tab.png)");
+		$("#register_tab").css("background","url(./img/login/login_tab2.png)");
+	});
+	$("#main").on("click","#login_tab",function(){
+		$("#login_tab").transition({y:0});
+		$("#register_tab").transition({y:0});
+		$("#login_tab").css("background","url(./img/login/login_tab2.png)");
+		$("#register_tab").css("background","url(./img/login/login_tab.png)");
+	});
 });
 function placeholder_hack(input){
 		if($(input).val() != ''){
